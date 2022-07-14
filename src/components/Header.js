32ce00af8ba4679b6e7c46b749aa1f6b95980485
Sarandeep Singh <70137752+SarandeepSingh17@users.noreply.github.com>
2022-07-14 15:15:02 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+import trollface from "./images/troll-face.png"
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src={trollface} 
+                className="header--image" alt="icon"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+        </header>
+    )
+}
